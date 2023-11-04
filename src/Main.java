5d0461258e3cee15530c3f9c36c26df91c489bcc
Лаисат <lz_256@mail.ru>
2023-11-05 00:29:40 +0300

    Conditional operator

--- src/Main.java
+++ src/Main.java
@@ -11,15 +11,16 @@ public class Main {
         var clientOS = 0;
         if (clientOS == 0) {
             System.out.println("cкачайте приложение для IOS");
-        } else {   if (clientOS == 1) {
+        } else {
+            if (clientOS == 1) {
                 System.out.println("cкачайте приложение для Android");
             } else {
-                    System.out.println(" IOS не поддерживается");
+                System.out.println(" IOS не поддерживается");
 
-                        }
-                    }
+            }
+        }
 
-                }
+    }
 
     public static void task2() {
         var year = 2018;
@@ -43,11 +44,11 @@ public class Main {
 
     public static void task3() {
         var year = 2021;
-        boolean isLeapYear = (year % 4 ==0 && year % 100 != 0 || year % 400 == 0) && year > 1584;
+        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) && year > 1584;
         if (isLeapYear) {
-            System.out.println( " год высокосный " + year );
+            System.out.println(" год высокосный " + year);
         } else {
-            System.out.println( " год НЕ высокосный " + year );
+            System.out.println(" год НЕ высокосный " + year);
         }
 
 
@@ -56,11 +57,11 @@ public class Main {
     public static void task4() {
         var deliveryDistance = 95;
         if (deliveryDistance < 20) {
-            System.out.println( "Потребуется " + deliveryDistance + " дней + срок доставки " );
+            System.out.println("Потребуется " + deliveryDistance + " дней + срок доставки ");
         } else if (deliveryDistance < 60) {
-            System.out.println( "Потребуется " + deliveryDistance + " дней + срок доставки " );
+            System.out.println("Потребуется " + deliveryDistance + " дней + срок доставки ");
         } else if (deliveryDistance < 100) {
-            System.out.println( "Потребуется " + deliveryDistance + " дней + срок доставки " );
+            System.out.println("Потребуется " + deliveryDistance + " дней + срок доставки ");
         } else {
             System.out.println("доставки нет");
 
@@ -68,7 +69,32 @@ public class Main {
     }
 
     public static void task5() {
-
-
+        var monthNumber = 3;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println( " Зима " + monthNumber );
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println( " Весна " + monthNumber );
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println( " Лето " + monthNumber );
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println( " Осень " + monthNumber );
+                break;
+            default:
+                System.out.println( " такой месяц не суцествует " + monthNumber );
+        }
     }
+
+
 }
